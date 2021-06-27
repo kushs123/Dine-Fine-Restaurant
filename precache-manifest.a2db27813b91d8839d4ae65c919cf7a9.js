5fc10946441aee6eed5368a653f732dc11fe7bcb
Kushagra Singh <kushagra.singhkushagra.singh@gmail.com>
2021-06-27 13:20:55 +0530

    Updates

---
+++ precache-manifest.a2db27813b91d8839d4ae65c919cf7a9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fad99681b13b3d3efd6",
+    "url": "/react-gh-pages/static/css/main.5860797e.chunk.css"
+  },
+  {
+    "revision": "6fad99681b13b3d3efd6",
+    "url": "/react-gh-pages/static/js/main.6fad9968.chunk.js"
+  },
+  {
+    "revision": "0192f8309dc578aa6751",
+    "url": "/react-gh-pages/static/css/1.272d8310.chunk.css"
+  },
+  {
+    "revision": "0192f8309dc578aa6751",
+    "url": "/react-gh-pages/static/js/1.0192f830.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-gh-pages/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "cb9cbe2a87f3c59020b91de9b16eed6e",
+    "url": "/react-gh-pages/index.html"
+  }
+];
